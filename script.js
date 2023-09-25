737074aa90e467918b29e5c5c55a77be458a708a
Rahul Yadav <119389899+Yadav123Rahul@users.noreply.github.com>
2023-09-25 20:53:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+ function removeSelectedColor() {
+            // Get a reference to the select element
+            var selectElement = document.getElementById("colorSelect");
+            
+            // Get the selected index
+            var selectedIndex = selectElement.selectedIndex;
+            
+            // Check if an option is selected
+            if (selectedIndex !== -1) {
+                // Remove the selected option from the list
+                selectElement.remove(selectedIndex);
+            }
+        }
